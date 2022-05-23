d9e3078267e55a8703091f86c660b2a12e2e600e
gabriegsti <gabrielgsdevbr@gmail.com>
2022-05-22 21:59:57 -0300

    first commit usuario não está fiel estou usando como uma classe teste para particularidades do aspnet core web api

---
+++ PlannerAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlannerAPI.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlannerAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private static List<Usuario> Usuarios = new List<Usuario>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaUsuario([FromBody] Usuario usuario)
+        {
+            usuario.Id = id++;
+            Usuarios.Add(usuario);
+            return CreatedAtAction(nameof(RecuperaUsuarioPorId), new { Id = usuario.Id }, usuario);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaUsuarios()
+        {
+            return Ok(Usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaUsuarioPorId(int id)
+        {
+            Usuario usuario =  Usuarios.FirstOrDefault(usuario => usuario.Id == id);
+            if (usuario != null)
+              return  Ok(usuario);
+            return NotFound();
+        }
+
+    }
+}
---
+++ PlannerAPI/Model/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlannerAPI.Model
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "O campo nome é obrigatório.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo email é obrigatório.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "O campo email deve ter entre 8 e 100 caracateres.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatório.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "O campo senha deve ter entre 8 e 100 caracateres.")]
+        public string Senha { get; set; }
+    }
+}
