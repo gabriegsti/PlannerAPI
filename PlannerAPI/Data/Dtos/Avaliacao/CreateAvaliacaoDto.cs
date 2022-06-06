d10f830510a93aa23082c45270edffe991cd4e54
gabriegsti <gabrielgsdevbr@gmail.com>
2022-06-06 20:43:47 -0300

    Ajuste para inserir nota em avaliacaDto

--- PlannerAPI/Data/Dtos/Avaliacao/CreateAvaliacaoDto.cs
+++ PlannerAPI/Data/Dtos/Avaliacao/CreateAvaliacaoDto.cs
@@ -10,7 +10,6 @@ namespace PlannerAPI.Data.Dtos.Avaliacao
         [Required]
         public string Titulo { get; set; }
         public DateTime Data_Hora { get; set; }
-        [Required]
-        public int Id_Evento { get; set; }
+        public double Nota { get; set; }
     }
 }
