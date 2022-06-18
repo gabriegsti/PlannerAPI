17846abaf4422760d3fb63237ed102807882ceec
LuizAlberto93 <costaesilva93@outlook.com>
2022-06-18 20:01:35 -0300

    subindo view materia e model home

--- PlannerAPI/Controllers/HomeController.cs
+++ PlannerAPI/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ namespace PlannerAPI.Controllers
             return View();
         }
 
+        public IActionResult Materia()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- PlannerAPI/Controllers/MateriaController.cs
+++ PlannerAPI/Controllers/MateriaController.cs
@@ -63,6 +63,7 @@ namespace PlannerAPI.Controllers
             return NotFound();
         }
 
+
     }
 
 }
